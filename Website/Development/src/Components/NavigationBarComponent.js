9349e2ab26b854588d63232e70aaac46845a1d7b
Mohammed Shahul Hameed <89022701+MD-Mohammed@users.noreply.github.com>
2022-07-11 14:20:38 +0530

    Navigation bar added

--- Website/Development/src/Components/NavigationBarComponent.js
+++ Website/Development/src/Components/NavigationBarComponent.js
@@ -7,44 +7,27 @@ function NavigationBarComponent() {
     return(
         <>
             <div className="navbar-main-container">
-
-                <div style={{"background-color": "black"}} className="navbar-logo-container">
-                    <div className="navbar-button-style">Logo</div>
+                <div className="navbar-logo-container">
+                    <div className="navbar-logo">
+                        <img style={{"height": "65%", "width": "65%", "flex": "1", "margin-bottom": "2%"}} src="/images/GEC-Logo.svg" />
+                    </div>
+                    <div className="navbar-logo-name">
+                        <span style={{"font-family": "'Montserrat', sans-serif", "font-size": "180%", "margin-left": "-15%", "font-weight": "500"}}>Gulf Erection</span>
+                        <span style={{"font-family": "'Montserrat', sans-serif", "margin-left": "-12.5%", "margin-top": "-5%", "letter-spacing": "6px", "font-weight": "300"}}>constructions</span>
+                    </div>
                 </div>
-
-                <div className="navbar-sub-container">
-                    <button className="navbar-button-style">Home</button>
+                <div className="navbar-buttons-main-container">
+                    <div className="navbar-buttons-container">
+                        <a href="#" className="navbar-buttons-style" >HOME</a>
+                        <a href="#" className="navbar-buttons-style" >ABOUT US</a>
+                        <a href="#" className="navbar-buttons-style" >SERVICES</a>
+                        <a href="#" className="navbar-buttons-style" >PROJECTS</a>
+                        <a href="#" className="navbar-buttons-style" >GALLERY</a>
+                        <a href="#" className="navbar-buttons-style" >CLIENT</a>
+                        <a href="#" className="navbar-buttons-style" >CONTACT US</a>
+                    </div>
                 </div>
-
-                <div className="navbar-sub-container">
-                    <button className="navbar-button-style">About us</button>
-                </div>
-
-                <div className="navbar-sub-container">
-                    <button className="navbar-button-style">Services</button>
-                </div>
-
-                <div className="navbar-sub-container">
-                    <button className="navbar-button-style">Projects</button>
-                </div>
-
-                <div className="navbar-sub-container">
-                    <button className="navbar-button-style">Gallery</button>
-                </div>
-
-                <div className="navbar-sub-container">
-                    <button className="navbar-button-style">Clients</button>
-                </div>
-
-                <div className="navbar-sub-container">
-                    <button className="navbar-button-style">Contact us</button>
-                </div>
-
-                <div style={{"flex" : "1", "display": "flex", "flex-direction": "row", "justify-content": "center", "align-items": "center"}} className="navbar-sub-container">
-                    <div style={{"background-color": "red", "flex": "2"}}>h</div>
-                    <div style={{"background-color": "white", "flex": "1"}}>h</div>
-                </div>
-
+                <div style={{"background-color": "black", "width" : "1%"}}>Button</div>
             </div>
         </>
     );
