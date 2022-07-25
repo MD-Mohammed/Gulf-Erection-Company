4083a30053c83b2211e6544017dfd5e7f31ee779
Mohammed Shahul Hameed <89022701+MD-Mohammed@users.noreply.github.com>
2022-07-25 09:31:46 +0530

    Framework Setup

---
+++ Website/Development/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, {Fragment, useState} from 'react';
+import { Routes,Route } from 'react-router-dom';
+
+import SampleComponent from './Components/SampleComponent.js'
+
+
+function App() {
+
+  return (
+
+    <SampleComponent/>
+
+    );
+}
+
+export default App;
---
+++ Website/Development/src/Components/SampleComponent.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import React, {Fragment} from "react";
+
+function SampleComponent() {
+
+    return(
+        <>
+
+            <div>Hello man</div>
+
+        </>
+    );
+} 
+export default SampleComponent;
