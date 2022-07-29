db3278af1499209ac79bb2d70962b308d4851817
Mohammed Shahul Hameed <89022701+MD-Mohammed@users.noreply.github.com>
2022-07-29 12:47:26 +0530

    About us page desktop fixed

--- Website/Development/src/Components/AboutUsPage.js
+++ Website/Development/src/Components/AboutUsPage.js
@@ -9,84 +9,97 @@ function AboutUsPage() {
     return(
             <>
  
-    <div className="aboutPage-main-container">
+    <div className="desktop-aboutus-page-main-container">
                     
-        <div className="aboutpage-main-container-block" style={{"flex":"1","flexDirection":"column"}}>
+        <div className="desktop-aboutus-page-header-container">
             
-            <div style={{"display":"flex", "margin-left": "-2%"}}>  
-                <img style={{"marginLeft":"20px","marginTop":"18px","height": "28px", "width": "31px"}} src={ process.env.PUBLIC_URL + "/images/aboutus-icon.png" }/>
+            <div style={{"display":"flex", "align-items": "center", "gap": "5%"}}>  
+                <img style={{"height": "12.5%", "width": "12.5%", "margin-top": "-2%"}} src={ process.env.PUBLIC_URL + "/images/aboutus-icon.png" }/>
                 
-                <h1 style={{"marginLeft":"20px","marginTop":"5px"}}>
+                <h1 style={{"font-family": "'Ubuntu', sans-serif", "font-weight": "400"}}>
                     ABOUT US
                 </h1>
             </div>
-            <div style={{"backgroundColor":"#FF4343","border":"5px","width":"147px","height":"5px","marginLeft":"-0.5%"}}>
-            </div>
+
+            <div className="desktop-aboutus-page-header-line" />
+
         </div>
 
-        <div className="aboutpage-main-container-block" style={{"flex":"8"}}>
+        <div className="desktop-aboutus-page-content-block">
             
-            <div className="aboutpage-sub-container-details" style={{"flex":"1"}}>
-                
-                <img className="aboutPage-img-prop" src={ process.env.PUBLIC_URL + "/images/aboutus-image.jpg" } />
+            <div className="desktop-aboutus-page-image-container">
+                <img className="desktop-aboutus-page-img-prop" src={ process.env.PUBLIC_URL + "/images/aboutus-image.jpg" } />
             </div>
             
-            <div className="aboutpage-sub-container-details" style={{"flex":"1", "flexDirection":"column","marginLeft":"50px","display":"flex","marginRight":"50px","marginTop":"30px", "justify-content": "space-between", "height": "82%" }}>
+            <div className="desktop-aboutus-page-content-container">
                 
-                <h2>
-                We meet your build expectations with exceptional quality 
-                </h2>
-                <a style={{"textAlign":"justify","fontSize":"20px","lineHeight":"23px"}}>
-                Being professionals with decades of experience competing in the space of steel structure buildings, we have maintained growth and prominence with the leading infrastructure and technology to meet the developing demands of the country.
-                </a>
+                <div className="desktop-aboutus-page-text-box">
 
-                <div style={{"display":"flex", "flexDirection":"row", "align-items": "center", "justify-content": "space-between"}}>
-                    <div style={{"display":"flex","flexDirection":"row", "align-items": "center", "width": "30%"}}>
-                        <img  className="aboutPage-badge-img-prop" src={ process.env.PUBLIC_URL + "/images/medal.png" } />
-                        <h2 className="aboutPage-badge-text" style={{"margin-top": "2%"}}>Trade License</h2>
+                    <h2> We meet your build expectations with exceptional quality </h2>
+
+                    <a style={{"text-align":"justify","font-size":"125%", "margin-right": "1%", "font-weight": "300"}}>
+                    Being professionals with decades of experience competing in the space of steel structure buildings, we have maintained growth and prominence with the leading infrastructure and technology to meet the developing demands of the country.
+                    </a>
+
+                </div>
+
+
+                <div className="desktop-aboutus-page-certificate-container">
+
+                    <div className="desktop-aboutus-page-badges">
+                        <img  className="desktop-aboutus-page-badge-icon" src={ process.env.PUBLIC_URL + "/images/medal.png" } />
+                        <h2 className="desktop-aboutus-page-badge-text" style={{"margin-top": "2%"}}>Trade License</h2>
                     </div>
-                    <div style={{"display":"flex","flexDirection":"row", "align-items": "center", "width": "30%", "justify-content": "center"}}>
-                        <img  className="aboutPage-badge-img-prop" src={ process.env.PUBLIC_URL + "/images/medal.png" } />
-                        <h2 className="aboutPage-badge-text" style={{"margin-top": "2%"}}>ISO Certificate</h2>
+
+                    <div className="desktop-aboutus-page-badges">
+                        <img  className="desktop-aboutus-page-badge-icon" src={ process.env.PUBLIC_URL + "/images/medal.png" } />
+                        <h2 className="desktop-aboutus-page-badge-text" style={{"margin-top": "2%"}}>ISO Certificate</h2>
                     </div>
-                    <div style={{"display":"flex","flexDirection":"row", "align-items": "center", "width": "30%", "justify-content": "flex-end"}}>
-                        <img  className="aboutPage-badge-img-prop" src={ process.env.PUBLIC_URL + "/images/medal.png" } />
-                        <h2 className="aboutPage-badge-text" style={{"margin-top": "2%"}}>ICV Certificate</h2>
+
+                    <div className="desktop-aboutus-page-badges">
+                        <img  className="desktop-aboutus-page-badge-icon" src={ process.env.PUBLIC_URL + "/images/medal.png" } />
+                        <h2 className="desktop-aboutus-page-badge-text" style={{"margin-top": "2%"}}>ICV Certificate</h2>
                     </div>
+
                 </div>
                 
-                <div style={{"display":"flex", "flexDirection":"row", "height": "10%"}}>
+                <div className="desktop-aboutus-page-buttons-container">
 
-                        <div className="aboutPage-button">
-                            <a href="#" className="aboutPage-badge-text" style={{"color": "white"}}>Read More</a>
-                        </div>
+                            <button className="desktop-aboutus-page-buttons" >Read More</button>
 
-                        <div style={{"backgroundColor":"#FF0000", "width": "40%", "margin-left": "10%"}} className="aboutPage-button">
-                            <a href="#" className="aboutPage-badge-text" style={{"color": "white"}}>Download Brochure</a>
-                        </div>
+                            <button className="desktop-aboutus-page-buttons" style={{"background-color": "#FF4343", "width": "45%", "margin-left": "5%"}} >Download Brochure</button>
                         
                 </div>
+
             </div>
         </div>
         
-        <div className="aboutpage-gridBox-main-container">
+        <div className="desktop-aboutus-page-grids-main-container">
             
-            <div className="aboutPage-sub-container-gridBox-text">
-                <h1>20+</h1>
-                <a>Years of experience</a>
-            </div>
-            <div className="aboutPage-sub-container-gridBox-text">
-                <h1>100+</h1>
-                <a>Projects completed</a>
-            </div>
-            <div className="aboutPage-sub-container-gridBox-text">
-                <h1>25+</h1>
-                <a>Satisified clients</a>
-            </div>
-            <div className="aboutPage-sub-container-gridBox-text">
-                <h1>300+</h1>
-                <a>Active workers</a>
-            </div>     
+            <div className="desktop-aboutus-page-grids-inner-container">
+            
+                <div className="desktop-abutus-page-sub-container-gridBox-text">
+                    <h1>20+</h1>
+                    <a style={{"font-weight": "200", "font-size": "110%"}}>Years of experience</a>
+                </div>
+
+                <div className="desktop-abutus-page-sub-container-gridBox-text">
+                    <h1>100+</h1>
+                    <a style={{"font-weight": "200", "font-size": "110%"}}>Projects completed</a>
+                </div>
+
+                <div className="desktop-abutus-page-sub-container-gridBox-text">
+                    <h1>25+</h1>
+                    <a style={{"font-weight": "200", "font-size": "110%"}}>Satisified clients</a>
+                </div>
+
+                <div className="desktop-abutus-page-sub-container-gridBox-text">
+                    <h1>300+</h1>
+                    <a style={{"font-weight": "200", "font-size": "110%"}}>Active workers</a>
+                </div>  
+
+            </div>   
+
         </div>
     
     </div>
