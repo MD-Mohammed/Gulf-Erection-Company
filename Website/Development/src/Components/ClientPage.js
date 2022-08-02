96f61f8bd43e4a85f6986313851f601cb5375967
Mohammed Shahul Hameed <102415951+Shejeer-Aboobacker@users.noreply.github.com>
2022-08-02 15:57:44 +0530

    commit
    Slider added

--- Website/Development/src/Components/ClientPage.js
+++ Website/Development/src/Components/ClientPage.js
@@ -1,17 +1,29 @@
 import { render } from "@testing-library/react";
 import React, {Fragment} from "react";
 import './ClientPage.css';
+import { motion } from "framer-motion"
+import { useState, useEffect,useRef} from "react"
+import images from "./Client_Images"
 
 function ClientPage() {
+    const [corouselwidth, setCorouselWidth]=useState(0);
+    const corousel=useRef();
+
+    useEffect(() =>{
+        setCorouselWidth(corousel.current.scrollWidth - corousel.current.offsetWidth);
+    }, []);
+    
+       
 
     function DesktopComponent() {
+       
 
 
     return(
             <>
             <div className="ClientPage-desk-Main-container">
                 
-                <div style={{"height":"10%"}}className="ClientPage-mobile-container">
+                <div style={{"height":"10%"}}>
                     
                      <div style={{"display":"flex"}}>  
                             <img style={{"marginLeft":"20px","marginTop":"18px","height": "28px", "width": "31px"}} src={ process.env.PUBLIC_URL + "/images/clients-icon.png" }/>
@@ -23,19 +35,24 @@ function ClientPage() {
                         <div style={{"backgroundColor":"#FF4343","border":"5px","width":"147px","height":"5px","marginLeft":"20px"}}>
                         </div>
                 </div>
-                <div style={{"height":"80%"}} className="ClientPage-desk-Main-container">
-                    <div className="ClientPage-desk-image-card-prop">
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    <img className="ClientPage-desk-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    
-                    </div>
+                <div style={{"height":"80%"}} className="ClientPage-desk-container">
+                    <div className="ClientPage-desk-App">
+                        <motion.div ref={corousel}className="ClientPage-desk-Main-Corousel">
+                            <motion.div drag ="x" dragConstraints ={{right:0,left: -corouselwidth}}className="ClientPage-desk-inner-Corousel">
+                                {
+                                    images.map(image => {
+                                        return(
+                                            <motion.div className="ClientPage-desk-item-image" key={image}> 
+                                                <img src={image} alt ="" />
+                                                
+                                            </motion.div>
+
+                                        );
+                                    })
+                                }
+                            </motion.div>
+                    </motion.div>
+                </div>
                 </div>
 
 
@@ -50,15 +67,14 @@ function MobileComponent() {
 
     return(
         <>
-
-<div className="ClientPage-mobile-Main-container">
+            <div className="ClientPage-mobile-Main-container">
                 
-                <div style={{"height":"10%"}}className="ClientPage-mobile-container">
+                <div style={{"height":"10%"}}>
                     
-                     <div style={{"display":"flex","AlignContent":"center"}}>  
+                     <div style={{"display":"flex"}}>  
                             <img style={{"marginLeft":"20px","marginTop":"18px","height": "28px", "width": "31px"}} src={ process.env.PUBLIC_URL + "/images/clients-icon.png" }/>
                             
-                            <h1 style={{"marginLeft":"20px","marginTop":"5%","color":"white","fontSize":"20px"}}>
+                            <h1 style={{"marginLeft":"20px","marginTop":"5px","color":"white"}}>
                             SUCCESSFUL ASSOCIATIONS
                             </h1>
                         </div>
@@ -66,28 +82,23 @@ function MobileComponent() {
                         </div>
                 </div>
                 <div style={{"height":"80%"}} className="ClientPage-mobile-container">
-                    <div className="ClientPage-mobile-image-card-prop">
-                    <img className="ClientPage-mobile-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    </div>
-                    <div className="ClientPage-mobile-image-card-prop">
-                    <img className="ClientPage-mobile-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    </div>
-                    <div className="ClientPage-mobile-image-card-prop">
-                    <img className="ClientPage-mobile-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    </div>
-                    <div className="ClientPage-mobile-image-card-prop">
-                    <img className="ClientPage-mobile-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    </div>
-                    <div className="ClientPage-mobile-image-card-prop">
-                    <img className="ClientPage-mobile-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    </div>
-                    <div className="ClientPage-mobile-image-card-prop">
-                    <img className="ClientPage-mobile-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    </div>
-                    <div className="ClientPage-mobile-image-card-prop">
-                    <img className="ClientPage-mobile-image-prop" src={ process.env.PUBLIC_URL + "/images/client-image.png" }/>
-                    </div>
+                    <div className="ClientPage-mobile-App">
+                        <motion.div ref={corousel}className="ClientPage-mobile-Main-Corousel">
+                            <motion.div drag ="x" dragConstraints ={{right:0,left: -corouselwidth}}className="ClientPage-mobile-inner-Corousel">
+                                {
+                                    images.map(image => {
+                                        return(
+                                            <motion.div className="ClientPage-mobile-item-image" key={image}> 
+                                                <img src={image} alt ="" />
+                                                
+                                            </motion.div>
 
+                                        );
+                                    })
+                                }
+                            </motion.div>
+                    </motion.div>
+                </div>
                 </div>
 
 
---
+++ Website/Development/src/Components/Client_Images.js
@@ -0,0 +1,10 @@
+import image1 from  "./images/client-image.png"
+import image2 from  "./images/client-image.png"
+import image3 from  "./images/client-image.png"
+import image4 from  "./images/client-image.png"
+import image5 from  "./images/client-image.png"
+import image6 from  "./images/client-image.png"
+import image7 from  "./images/client-image.png"
+import image8 from  "./images/client-image.png"
+
+export default[image1,image2,image3,image4,image5,image6,image7,image8]
